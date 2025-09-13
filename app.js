7bc3ee74bf842d484b8ee163370494f476d5ed39
supreMe7492 <supremeraut94@gmail.com>
2025-09-13 21:54:26 +0545

    just some checking

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("node:path");
+const app = express();
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/',(req,res)=>{
+    res.send("this is home page")
+});
+
+app.listen(3000,()=>{
+    console.log("check it out on the port 3000")
+})
